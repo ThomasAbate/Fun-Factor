a85ec1fdd71e2935de0ff1f5ee7cdce7d29b79e9
ThAb <117899380+ThomasAbate@users.noreply.github.com>
2023-03-11 17:13:49 +0100

    first commit

---
+++ Assets/C/Wapon/Force.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Force : MonoBehaviour
+{
+
+    [SerializeField]
+    private Racket weapon;
+
+    [SerializeField]
+    private string enemyTag;
+
+    [SerializeField]
+    private float maxFirePower;
+
+    [SerializeField]
+    private float firePowerSpeed;
+
+    private float firePower;
+
+    [SerializeField]
+    private float rotateSpeed;
+
+    [SerializeField]
+    private float minRotation;
+
+    [SerializeField]
+    private float maxRotation;
+
+    private float mouseY;
+
+    private bool fire;
+
+    void Start()
+    {
+        weapon.SetEnemyTag(enemyTag);
+        weapon.Reload();
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        mouseY -= Input.GetAxis("Mouse Y") * rotateSpeed;
+        mouseY = Mathf.Clamp(mouseY, minRotation, maxRotation);
+        weapon.transform.localRotation = Quaternion.Euler(mouseY, weapon.transform.localEulerAngles.y, weapon.transform.localEulerAngles.z);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            fire = true;
+        }
+
+        if (fire && firePower < maxFirePower)
+        {
+            firePower += Time.deltaTime * firePowerSpeed;
+        }
+
+        if (fire && Input.GetMouseButtonUp(0))
+        {
+            weapon.Fire(firePower);
+            firePower = 0;
+            fire = false;
+        }
+
+    }
+}
---
+++ Assets/C/Wapon/Racket.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour
+{
+    [SerializeField]
+    private float reloadTime;
+
+    [SerializeField]
+    private Balls BallsPrefab;
+
+    [SerializeField]
+    private Transform spawnPoint;
+
+    private Balls currentArrow;
+
+    private string enemyTag;
+
+    private bool isReloading;
+
+    public void SetEnemyTag(string enemyTag)
+    {
+        this.enemyTag = enemyTag;
+    }
+
+    public void Reload()
+    {
+        if (isReloading || currentArrow != null) return;
+        isReloading = true;
+        StartCoroutine(ReloadAfterTime());
+    }
+
+    private IEnumerator ReloadAfterTime()
+    {
+        yield return new WaitForSeconds(reloadTime);
+        currentArrow = Instantiate(BallsPrefab, spawnPoint);
+        currentArrow.transform.localPosition = Vector3.zero;
+        currentArrow.SetEnemyTag(enemyTag);
+        isReloading = false;
+    }
+
+    public void Fire(float firePower)
+    {
+        if (isReloading || currentArrow == null) return;
+        var force = spawnPoint.TransformDirection(Vector3.forward * firePower);
+        currentArrow.Fly(force);
+        currentArrow = null;
+        Reload();
+    }
+
+    public bool IsReady()
+    {
+        return (!isReloading && currentArrow != null);
+    }
+}
